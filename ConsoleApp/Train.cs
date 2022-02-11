af758542c22c936b8cfb85bc06f91da53e9ffdba
Dmytro <dim.fed.13@gmail.com>
2022-02-11 21:59:27 +0200

    In class Train created 7 parameters and construction

--- ConsoleApp/Train.cs
+++ ConsoleApp/Train.cs
@@ -8,5 +8,23 @@ namespace ConsoleApp
 {
     public class Train:Vehicle
     {
+        public string Type { get; set; }
+        public string NameTrain { get; set; }
+        public string ColorTrain { get; set; }
+        public int Kilometrage { get; set; }
+        public string City { get; set; }
+        public string Settlement { get; set; }
+        public int Distance { get; set; }
+
+        public Train(string type, string nametrain, string colortrain, int kilometrage, string city, string settlement, int distance)
+        {
+            Type = type;
+            NameTrain = nametrain;
+            ColorTrain = colortrain;
+            Kilometrage = kilometrage;
+            City = city;
+            Settlement = settlement;
+            Distance = distance;
+        }
     }
 }
