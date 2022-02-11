6759a423a68636237047c024d2bb1108ce2e86d1
Dmytro <dim.fed.13@gmail.com>
2022-02-11 21:37:25 +0200

    In class Car created 3 parameters and construction

--- ConsoleApp/Car.cs
+++ ConsoleApp/Car.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    internal class Car
+    public class Car
     {
+        public string Type { get; set; }
+        public string NameCar { get; set; }
+        public string ColorCar { get; set; }
+        public int Kilometrage { get; set; }
+        public string City { get; set; }
+        public string Settlement { get; set; }
+        public int Distance { get; set; }
+
+        public Car(string type, string namecar, string colorcar, int kilometrage, string city, string settlement, int distance)
+        {
+            Type = type;
+            NameCar = namecar;
+            ColorCar = colorcar;
+            Kilometrage = kilometrage;
+            City = city;
+            Settlement = settlement;
+            Distance = distance;
+        }
     }
 }
