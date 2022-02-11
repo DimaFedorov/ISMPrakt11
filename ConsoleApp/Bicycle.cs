51af657d7af694c11b15bc94d3e3a1041e421d1c
Dmytro <dim.fed.13@gmail.com>
2022-02-11 22:02:10 +0200

    In class Bicycle created 7 parameters and construction

--- ConsoleApp/Bicycle.cs
+++ ConsoleApp/Bicycle.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    internal class Bicycle
+    public class Bicycle
     {
+        public string Type { get; set; }
+        public string NameBicycle { get; set; }
+        public string ColorBicycle { get; set; }
+        public int Kilometrage { get; set; }
+        public string City { get; set; }
+        public string Settlement { get; set; }
+        public int Distance { get; set; }
+        public Bicycle(string type, string namebicycle, string colorbicycle, int kilometrage, string city, string settlement, int distance)
+        {
+            Type = type;
+            NameBicycle = namebicycle;
+            ColorBicycle = colorbicycle;
+            Kilometrage = kilometrage;
+            City = city;
+            Settlement = settlement;
+            Distance = distance;
+        }
     }
 }
